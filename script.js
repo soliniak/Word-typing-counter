0658649e1997a7160b37c9634902739d24899cf4
Rafi <soliniakdlagier@gmail.com>
2019-02-04 10:51:33 +0100

    Added average words per second

--- script.js
+++ script.js
@@ -1,152 +1,166 @@
-$(function(){
+$(function() {
+  let iG = 0;
+  let wrongAnswerCounter;
+  let stop = false,
+    startTimer = false;
 
+  function callback(value, value2) {
+    textInput = value2;
+    textInputArray = value;
+    return textInputArray, textInput;
+  }
 
-	let iG = 0;
-	let wrongAnswerCounter;
-	let stop = false,
-		startTimer = false;
+  $(".textOutput").keypress(check);
 
-	function callback(value, value2){ 
-		textInput = value2;
-		textInputArray = value;
-		return textInputArray, textInput; 
-	};
-	
-	$(".textOutput").keypress(check);
+  // getting text from db when start is clicked
+  $(".button--start").on("click", function() {
+    reseting();
 
-// getting text from db when start is clicked
-	$(".button--start").on("click", function (){
+    $.getJSON("text_db.json", function(json) {
+      let x = Math.floor(Math.random() * Object.keys(json).length);
+      let $selected = $("#selectLanguage option:selected").val();
+      let textInput = json[$selected][x];
 
-		reseting();
-		
-		$.getJSON('text_db.json', function (json){
+      let $textOutput = $(".textOutput").val();
+      let textInputArray = textInput.split(" ");
+      let textInputArraySave = textInput.split(" ");
 
-			let x = Math.floor(Math.random() * Object.keys(json).length);
-			let $selected = $("#selectLanguage option:selected").val();
-			let textInput = json[$selected][x];
+      for (var i = 0; i < textInputArray.length; i++) {
+        textInputArraySave[i] =
+          '<span id="x' + i + '">' + textInputArray[i] + "</span>";
+        textInput = textInputArraySave.join(" ");
+      }
+      callback(textInputArray, textInput);
+      $(".textInput").html(textInput);
 
-			let $textOutput = $(".textOutput").val();
-			let textInputArray = textInput.split(" ");
-			let textInputArraySave = textInput.split(" ");
-			
-			for(var i = 0; i < textInputArray.length; i++){
-				textInputArraySave[i] = "<span id=\"x"+i+"\">" + textInputArray[i] + "</span>";
-		        textInput = textInputArraySave.join(" ");
-			}
-			callback(textInputArray, textInput);
-			$(".textInput").html(textInput);
-			
-			words(iG, textInputArray);
-			highlight(iG);
-			updateCounter();
-		});
-	});
-	
-	
-// checking if spacebar or enter is pressed then comparing words. If error update counter, if words match go to next
-	function check(e, callback) {
+      words(iG, textInputArray);
+      highlight(iG);
+      updateCounter();
+    });
+  });
 
-		if(!startTimer){
-			startTimer = true;
-			timer(startTimer);
-		}
+  // checking if spacebar or enter is pressed then comparing words. If error update counter, if words match go to next
+  function check(e, callback) {
+    if (!startTimer) {
+      startTimer = true;
+      timer(startTimer);
+    }
 
-	    if (e.which === 32 || e.which === 13) {
-	    	e.preventDefault();
-			
-			let $textOutput = $(".textOutput").val();
+    if (e.which === 32 || e.which === 13) {
+      e.preventDefault();
 
-			if($textOutput === textInputArray[iG]){
-				$(".textOutput").val("");
-				iG++;
-		    	highlight(iG, textInputArray[iG], $textOutput);
-		    	words(iG, textInputArray);
-		    	typerState();
-		    } else {
-				wrongAnswerCounter += 1;
-				updateCounter(wrongAnswerCounter);
-			}
-			typerState();
-	    }
-	}
+      let $textOutput = $(".textOutput").val();
 
-// Counting wrong answers (counts only when spacebar or enter is hit and words don't match)
-	function updateCounter(){
-		$(".wrongAnswer").text(wrongAnswerCounter);
-	}
+      if ($textOutput === textInputArray[iG]) {
+        $(".textOutput").val("");
+        iG++;
+        highlight(iG, textInputArray[iG], $textOutput);
+        words(iG, textInputArray);
+        typerState();
+      } else {
+        wrongAnswerCounter += 1;
+        updateCounter(wrongAnswerCounter);
+      }
+      typerState();
+    }
+  }
 
+  // Counting wrong answers (counts only when spacebar or enter is hit and words don't match)
+  function updateCounter() {
+    $(".wrongAnswer").text(wrongAnswerCounter);
+  }
 
-// Highlihting correct word and one ahead
-	function highlight(iG){
-		$("#x"+iG).addClass("nextWord");
-	    iG = iG -1;
-	    $("#x"+iG).removeClass("nextWord");
-	    $("#x"+iG).addClass("correctWord");
-	}
+  // Highlihting correct word and one ahead
+  function highlight(iG) {
+    $("#x" + iG).addClass("nextWord");
+    iG = iG - 1;
+    $("#x" + iG).removeClass("nextWord");
+    $("#x" + iG).addClass("correctWord");
+  }
 
-// Changing Typer state active / disabled
-	function typerState(){
-	    if(iG >= textInputArray.length){
-    		console.log("End");
-    		stop = true;
-    		$(".button--start").removeAttr("disabled", " ");
-    		$("#selectLanguage").removeAttr("disabled", " ")
-    		$("textarea.textOutput").attr("disabled", "disabled");
-    		iG = 0;
-    		return;
-		}   
-	}
+  // Changing Typer state active / disabled
 
-// setting timer
-	function timer(startTimer){
-		let ms = 0, sec = 0, min = 0, hour = 0, msAdd, secAdd, minAdd, hourAdd, czas;
-		
-		if(startTimer == true){
-			let myInt = setInterval(function() {
-					msAdd = ms < 10 ? "0" + ms : ms;
-					secAdd = sec < 10 ? "0" + sec : sec;
-					minAdd = min < 10 ? "0" + min : min;
-					hourAdd = hour < 10 ? "0" + hour : hour;
-				if(!stop){
-					ms++;
-					if(ms == 100){
-						ms = 0;
-						sec++;
-					}
-					if(sec == 60){
-						min++;
-						sec = 0;
-					}
-					if(min == 60){
-						hour++;
-						min = 0;
-					}
-					refresh();
-				}else{
-					czas = hourAdd + ":" + minAdd + ":" + secAdd + ":" + msAdd;
-					console.log("Your time: " + czas + " Errors: " + wrongAnswerCounter);
-					clearInterval(myInt);
-				}
-			}, 10);
-		}
+  let averageSeconds = 0;
 
-		function refresh(){
-			let czas = hourAdd + ":" + minAdd + ":" + secAdd + ":" + msAdd;
-			$(".timer").text(czas);
-		}
-	}
+  function typerState() {
+    if (iG >= textInputArray.length) {
+      console.log("End");
+      average(averageSeconds, textInputArray);
+      stop = true;
+      $(".button--start").removeAttr("disabled", " ");
+      $("#selectLanguage").removeAttr("disabled", " ");
+      $("textarea.textOutput").attr("disabled", "disabled");
+      iG = 0;
+      return;
+    }
+  }
 
-// reseting when writing is done
-	function reseting(){
-		$("textarea.textOutput").removeAttr("disabled", "disabled");
-		$(".button--start").attr("disabled", "disabled");
-		$("#selectLanguage").attr("disabled", "disabled")
-		wrongAnswerCounter = 0;
-		stop = false;
-		startTimer = false;
-		updateCounter(wrongAnswerCounter);
-	}
-	function words(iG, textInputArray){
-		$(".words").text("Words: " + iG + " / " + textInputArray.length)
-	}
+  // setting timer
+  function timer(startTimer) {
+    let ms = 0,
+      sec = 0,
+      min = 0,
+      hour = 0,
+      msAdd,
+      secAdd,
+      minAdd,
+      hourAdd,
+      time;
+
+    if (startTimer == true) {
+      let myInt = setInterval(function() {
+        msAdd = ms < 10 ? "0" + ms : ms;
+        secAdd = sec < 10 ? "0" + sec : sec;
+        minAdd = min < 10 ? "0" + min : min;
+        hourAdd = hour < 10 ? "0" + hour : hour;
+        if (!stop) {
+          ms++;
+          averageSeconds++;
+
+          if (ms == 100) {
+            ms = 0;
+            sec++;
+          }
+          if (sec == 60) {
+            min++;
+            sec = 0;
+          }
+          if (min == 60) {
+            hour++;
+            min = 0;
+          }
+          refreshTime();
+        } else {
+          time = hourAdd + ":" + minAdd + ":" + secAdd + ":" + msAdd;
+          console.log("Your time: " + time + " Errors: " + wrongAnswerCounter);
+          clearInterval(myInt);
+        }
+      }, 10);
+    }
+
+    function refreshTime() {
+      let time = hourAdd + ":" + minAdd + ":" + secAdd + ":" + msAdd;
+      $(".timer").text(time);
+    }
+  }
+
+  // calc average words per seconds
+  function average(averageSeconds, textInputArray) {
+    const av = averageSeconds / 100 / textInputArray.length;
+    $(".average").text("Average: " + av.toFixed(2) + " words / second");
+  }
+  // reseting when writing is done
+  function reseting() {
+    $("textarea.textOutput").removeAttr("disabled", "disabled");
+    $(".button--start").attr("disabled", "disabled");
+    $("#selectLanguage").attr("disabled", "disabled");
+    wrongAnswerCounter = 0;
+    stop = false;
+    startTimer = false;
+    averageSeconds = 0;
+    updateCounter(wrongAnswerCounter);
+  }
+  function words(iG, textInputArray) {
+    $(".words").text("Words: " + iG + " / " + textInputArray.length);
+  }
 });
